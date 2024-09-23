5225f7b5e4371f96045c4a8fd46b07b7b840302a
Tuan Anh <anhtranduy25@gmail.com>
2024-09-23 09:43:46 +0700

    fix commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
@@ -91,4 +92,5 @@ def predict_price():
         return f"Đã có lỗi xảy ra: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
